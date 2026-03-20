66ad910b721e6d9a0489cc71b804249d88caff75
Joaquin <joaquinpereiraheran@gmail.com>
2026-03-19 23:49:42 -0300

    link

---
+++ while.js
@@ -0,0 +1,18 @@
+const usuario = prompt("Ingrese su usuario").toLowerCase();
+const clave = prompt("ingrese su clave");
+
+for (let i = 0; i < 3; i++) {
+
+    if (usuario === "joaco" && clave === "pepe") {
+        alert("todo oki bro");
+        break;
+    }
+    else (
+        alert("so tonto ??")
+ 
+    )
+}
+
+
+
+
